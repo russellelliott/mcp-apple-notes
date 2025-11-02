9cb819552bf77f4d1897418b6c5b63b0f9fe6c93
Russell Elliott <russell@quadsquad.net>
2025-11-01 17:08:18 -0700

    clenaed out a TON of test files previously made

--- cli.sh
+++ cli.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Index the Notes
-bun cli.ts --max=10 --mode=fresh
+bun cli.ts --max=100 --mode=fresh
+
+# Might as well sync the cache if needed
+bun sync-db-cache.ts
 
 # Perform Clustering on the Notes and display them
 bun cluster-and-display.ts
