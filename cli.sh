368f86ad5ceab6281f377e62959650bf3352d3e5
Russell Elliott <russell@quadsquad.net>
2026-01-22 19:54:20 -0800

    clean up command

--- cli.sh
+++ cli.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # Index the Notes
-# bun cli.ts --max=200 --mode=fresh
-# bun cli.ts --max=200 --mode=fresh
-
-
-# Might as well sync the cache if needed
-bun sync-db-cache.ts
+# bun cli.ts --mode=incremental-since
+# bun cli.ts --max=200 --mode=incremental
